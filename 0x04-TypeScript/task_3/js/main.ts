a59e4af0fe4093321a811c0ec012dcd207ace2b2
Fatiha Osman <your-email@example.com>
2025-11-07 14:55:50 +0000

    done with task-8

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -14,8 +14,7 @@ const newRowID: RowID = CRUD.insertRow(row);
 
 // Update the row
 const updatedRow: RowElement = {
-  ...row,
-  age: 23
+  ...row, age: 23
 };
 
 CRUD.updateRow(newRowID, updatedRow);
