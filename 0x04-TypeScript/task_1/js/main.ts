271cd3f63da07fef4cf0a544cf9a487fb7712195
Fatiha Osman <your-email@example.com>
2025-11-07 06:14:07 +0000

    resolving2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -26,8 +26,7 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
- function printTeacher(firstName: string;
- lastName: string): string {
+ function printTeacher(firstName: string; lastName: string): string {
   $return `${firstName.charAt(0)}. ${lastName}`;
 };
 
